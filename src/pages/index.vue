a5bf53a78b3e8aa407f4b4ce0849722452a33ddc
lopburny <lopburny@riotz.works>
2020-05-04 15:58:17 +0000

    Add link for sample page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,53 @@
 <template>
-  <div />
+  <div>
+    <v-row>
+      <v-col sm="6">
+        <v-card
+          outlined
+          min-width="300"
+        >
+          <v-card-text class="text-center">
+            メンバー登録
+          </v-card-text>
+          <v-divider />
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="primary"
+              text
+              @click="toRegistrationPage"
+            >
+              サンプルページへ移動
+            </v-btn>
+            <v-spacer />
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col sm="6">
+        <v-card
+          outlined
+          min-width="300"
+        >
+          <v-card-text class="text-center">
+            メンバーの一覧ダウンロード
+          </v-card-text>
+          <v-divider />
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="primary"
+              text
+              @click="toDownloadPage"
+            >
+              サンプルページへ移動
+            </v-btn>
+            <v-spacer />
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 
@@ -7,9 +55,15 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  created() {
-    this.$router.push({ path: '/registration' })
-  },
+  layout: 'centering',
+  methods: {
+    toRegistrationPage() {
+      this.$router.push({ path: '/registration' })
+    },
+    toDownloadPage() {
+      this.$router.push({ path: '/download' })
+    }
+  }
 })
 </script>
 
